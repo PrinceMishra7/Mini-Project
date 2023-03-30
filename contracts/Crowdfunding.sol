944c48d8b30d8d3909d6abff63b05d84089642d4
Prince <prince.mishra@spit.ac.in>
2023-03-30 20:55:25 +0530

    smart contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,79 +1,151 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 contract Crowdfunding {
-    address public campaignOwner;
-    uint public campaignGoal;
-    uint public totalDonated;
-    uint public votingEndTime;
-    uint public donationEndTime;
-    uint public yesVotes;
-    uint public noVotes;
-    bool public votingEnded;
-    bool public donationEnabled;
-
-    mapping(address => bool) public backers;
-    mapping(address => uint) public donations;
-
-    event CampaignStarted(address indexed owner, uint goal, uint votingEnd, uint donationEnd);
-    event VoteCasted(address indexed backer, bool choice);
-    event DonationMade(address indexed backer, uint amount);
-
-    modifier onlyOwner() {
-        require(msg.sender == campaignOwner, "Only the campaign owner can call this function.");
-        _;
+    struct Campaign {
+        string title;
+        string description;
+        address seeker;
+        uint goal;
+        uint raised;
+        bool approved;
+        bool completed;
+        bool voteApproved;
+        uint256 phase;
+        bool rejected;
+        address[] donators;
+        uint256[] donations;
+        address[] voters;
+        uint256[] votes;
+    }
+    address public admin;
+    
+    mapping(uint => Campaign) public campaigns;
+    uint public campaignsCount;
+    
+    constructor() {
+        admin = msg.sender;
+        campaignsCount = 0;
+    }
+    
+    function createCampaign(address _seeker,string memory _title, string memory _description, uint _goal) public returns(uint256){
+        Campaign storage campaign=campaigns[campaignsCount];
+        campaign.seeker=_seeker;
+        campaign.title=_title;
+        campaign.description=_description;
+        campaign.approved=false;
+        campaign.completed=false;
+        campaign.goal=_goal;
+        campaign.voteApproved=false;
+        campaign.raised=0;
+        campaign.phase=0;
+        campaign.rejected=false;
+        campaignsCount++;
+        return campaignsCount;
     }
 
-    modifier votingPeriod() {
-        require(block.timestamp <= votingEndTime, "Voting period has ended.");
-        _;
+    function approveCampaign(uint256 _id) public {
+        require(msg.sender == admin, "Only admin can approve campaigns");
+        Campaign storage campaign = campaigns[_id];
+        campaign.approved=true;
     }
 
-    modifier donationPeriod() {
-        require(donationEnabled, "Donation period has not yet started.");
-        require(block.timestamp <= donationEndTime, "Donation period has ended.");
-        _;
+    function rejectCampaign(uint256 _id) public {
+        require(msg.sender == admin, "Only admin can approve campaigns");
+        Campaign storage campaign = campaigns[_id];
+        campaign.rejected=true;
     }
 
-    function startCampaign(uint _campaignGoal, uint _votingDuration, uint _donationDuration) public {
-        require(!votingEnded, "Voting has already ended.");
-        require(_campaignGoal > 0, "Campaign goal must be greater than zero.");
-        require(_votingDuration > 0, "Voting duration must be greater than zero.");
-        require(_donationDuration > 0, "Donation duration must be greater than zero.");
-
-        campaignOwner = msg.sender;
-        campaignGoal = _campaignGoal;
-        votingEndTime = block.timestamp + _votingDuration;
-        donationEndTime = votingEndTime + _donationDuration;
-        votingEnded = false;
-        donationEnabled = false;
-        
-        emit CampaignStarted(campaignOwner, campaignGoal, votingEndTime, donationEndTime);
+    function startVoting(uint256 _id) public{
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == admin, "Only admin can start voting");
+        require(campaign.approved == true, "Need to be approved by admin first");
+        campaign.phase=1;
     }
 
-    function castVote(bool _choice) public votingPeriod {
-        require(!backers[msg.sender], "You have already voted.");
-        if (_choice) {
-            yesVotes++;
-        } else {
-            noVotes++;
+    function makeVote(uint256 _id,uint256 _val) public{
+        Campaign storage campaign = campaigns[_id];
+        require(campaign.phase == 1, "Voting closed");
+        campaign.voters.push(msg.sender);
+        campaign.votes.push(_val);
+    }
+
+    function endVoting(uint256 _id) public{
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == admin, "Only admin can end voting");
+        require(campaign.phase == 1, "Voting not yet started");
+        campaign.phase=2;
+    }
+
+  
+
+    function votingResult(uint256 _id) public{
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == admin, "Only admin can declare result");
+        require(campaign.phase == 2, "Voting not yet closed");
+        campaign.phase=3;
+        uint256 len=campaign.votes.length;
+        uint256 posVote=0;
+        for(uint256 i=0;i<len;i++){
+            if(campaign.votes[i]==1){
+                posVote++;
+            }
         }
-        backers[msg.sender] = true;
-        emit VoteCasted(msg.sender, _choice);
+        if(posVote>=len/2){
+            campaign.voteApproved=true;
+            
+        }
+        else{
+            campaign.rejected=true;
+            
+        }
+    }
+    function donateToCampaign(uint256 _id) public payable{
+        Campaign storage campaign = campaigns[_id];
+        require(campaign.phase == 3,"Voting result not yet declared");
+        require(campaign.rejected==false,"This Campaign has been rejected");
+
+        uint256 amount = msg.value;
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+        (bool sent,) = payable(campaign.seeker).call{value: amount}("");
+        if(sent) {
+            campaign.raised = campaign.raised + amount;
+        }
     }
 
-    function startDonation() public onlyOwner votingPeriod {
-        require(!votingEnded, "Voting has already ended.");
-        votingEnded = true;
-        donationEnabled = yesVotes > noVotes;
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function makeDonation() public payable donationPeriod {
-        require(donationEnabled, "Donations are not enabled yet.");
-        require(!backers[msg.sender], "You have already donated.");
-        backers[msg.sender] = true;
-        donations[msg.sender] = msg.value;
-        totalDonated += msg.value;
-        payable(campaignOwner).transfer(msg.value);
-        emit DonationMade(msg.sender, msg.value);
+    function getCampaignPhase(uint256 _id) public view returns(string memory){
+        Campaign storage campaign=campaigns[_id];
+        uint status=campaign.phase;
+        string memory message="";
+        if(status==0){
+            if(campaign.rejected==true) message="Rejected by admin";  
+            else message="not yet approved by admin";   
+        }
+        else if(status==1){
+            message="admin approved, in voting phase";
+        }
+        else if(status==2){
+             message="admin approved, voting has closed";   
+        }
+        else if(status==3){
+            if(campaign.rejected==true) message="admin approved, rejected by people"; 
+            else if (campaign.voteApproved==true) message="admin approved, donation phase has started"; 
+        }
+        return message;
     }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](campaignsCount);
+        for(uint i = 0; i < campaignsCount; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
+
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,14 @@
 const hre = require("hardhat");
-
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
   const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
   const crowdfunding = await Crowdfunding.deploy();
 
   await crowdfunding.deployed();
 
-  console.log("Library deployed to:", crowdfunding.address);
+  console.log("Contract deployed to:", crowdfunding.address);
 }
 
 main().catch((error) => {
